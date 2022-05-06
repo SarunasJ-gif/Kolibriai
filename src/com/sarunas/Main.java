94f977b9258dd79f3a2b103ead45f9c7b1f4a0e6
SarunasJ-gif <sarunasj82@gmail.com>
2022-05-06 20:22:51 +0300

    counting all steps

--- src/com/sarunas/Main.java
+++ src/com/sarunas/Main.java
@@ -50,66 +50,83 @@ public class Main {
 
 
     public static void game(char[][] board) {
+        int rounds = 0;
+        int countRight = 0;
+        int countRightBreak = 0;
+        int countDown = 0;
+        int countDownBreak = 0;
 
-//     boolean flag = true;
-//     while (flag) {
-
-
-// moving right >
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == '>') {                                 //goes right
-                    if (board[i][(j + 1) % board[i].length] == '.') {
-                        char tempJ = board[i][j];
-                        board[i][j] = board[i][(j + 1) % board[i].length];
-                        board[i][(j + 1) % board[i].length] = tempJ;
-                        break;
-                    } else if (board[i][(j + 1) % board[i].length] == 'x') {
-                        int p = j;
-                        int e = j;
-                        while (board[i][(e + 1) % board[i].length] == 'x') {
-                            e++;
+        while (true) {
+            for (int i = 0; i < board.length; i++) {                         // moving right >
+                for (int j = 0; j < board[i].length; j++) {
+                    if (board[i][j] == '>') {
+                        countRight++;
+                        if (board[i][(j + 1) % board[i].length] == '.') {
+                            char tempJ = board[i][j];
+                            board[i][j] = board[i][(j + 1) % board[i].length];
+                            board[i][(j + 1) % board[i].length] = tempJ;
+                            break;
+                        } else if (board[i][(j + 1) % board[i].length] == 'x') {
+                            int p = j;
+                            int e = j;
+                            while (board[i][(e + 1) % board[i].length] == 'x') {
+                                e++;
+                            }
+                            char tempJX = board[i][p];
+                            board[i][p] = board[i][(e + 1) % board[i].length];
+                            board[i][(e + 1) % board[i].length] = tempJX;//  to escape 'x'
+                            break;
+                        } else if (board[i][(j + 1) % board[i].length] == 'v' || board[i][(j + 1) % board[i].length] == '>') {
+                            countRightBreak++;
+                            break;
                         }
-                        char tempJX = board[i][p];
-                        board[i][p] = board[i][(e + 1) % board[i].length];
-                        board[i][(e + 1) % board[i].length] = tempJX;//  to escape 'x'
-                        break;
-                    } else if (board[i][(j + 1) % board[i].length] == 'v' || board[i][(j + 1) % board[i].length] == '>') {
-                        break;
                     }
                 }
             }
+
+            if ( countRight == countRightBreak) {
+                break;
+            } else {
+                countRight = 0;
+                countRightBreak = 0;
+            }
+
+            for (int b = 0; b < board[0].length; b++) {                        // moving down v
+                for (int a = 0; a < board.length; a++) {
+                    if (board[a][b] == 'v') {
+                        countDown++;
+                        if (board[(a + 1) % board.length][b] == '.') {
+                            char tempI = board[a][b];
+                            board[a][b] = board[(a + 1) % board.length][b];
+                            board[(a + 1) % board.length][b] = tempI;
+                            break;
+                        } else if (board[(a + 1) % board.length][b] == 'x'){
+                            int o = a;
+                            int p = a;
+                            while (board[(p + 1) % board.length][b] == 'x') {
+                                p++;
+                            }
+                            char tempIX = board[o][b];
+                            board[o][b] = board[(p + 1) % board.length][b];
+                            board[(p + 1) % board.length][b] = tempIX;
+                            break;
+                        } else if (board[(a + 1) % board.length][b] == '>' || board[(a + 1) % board.length][b] == 'v') {
+                            countDownBreak++;
+                            break;
+                        }
+                    }
+                }
+           }
+
+           if ( countDown == countDownBreak) {
+               break;
+           } else {
+               countDown = 0;
+               countDownBreak = 0;
+           }
+
+            rounds++;
         }
-
-
-        for (int b = 0; b < board[0].length; b++) {
-            for (int a = 0; a < board.length; a++) {
-                if (board[a][b] == 'v') {
-                    if (board[(a + 1) % board.length][b] == '.') {
-                        char tempI = board[a][b];
-                        board[a][b] = board[(a + 1) % board.length][b];
-                        board[(a + 1) % board.length][b] = tempI;
-                        break;
-                    } else if (board[(a + 1) % board.length][b] == 'x'){
-                        int o = a;
-                        int p = a;
-                        while (board[(p + 1) % board.length][b] == 'x') {
-                            p++;
-                        }
-                        char tempIX = board[o][b];
-                        board[o][b] = board[(p + 1) % board.length][b];
-                        board[(p + 1) % board.length][b] = tempIX;
-                        break;
-                    } else if (board[(a + 1) % board.length][b] == '>' || board[(a + 1) % board.length][b] == 'v') {        // stops if >
-                        break;
-                    }
-                }
-            }
-       }
-
-
-              //  }
 
 
 
@@ -122,6 +139,7 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println(rounds);
     }
 
 
